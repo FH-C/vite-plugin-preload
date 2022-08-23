f2d1ab24418f2c503581719b05929f2fb58c49c0
fh_c <fh_c@qq.com>
2022-08-23 10:07:14 +0800

    feat: first commit

---
+++ src/index.ts
@@ -0,0 +1,55 @@
+const fs = require("fs");
+const path = require("path");
+
+const fileList = new Set<string>();
+
+const getSubDirectory = (_path: string) => {
+  if (fs.lstatSync(_path).isDirectory()) {
+    const files = fs.readdirSync(_path);
+    files.forEach((file: string) => {
+      getSubDirectory(path.resolve(_path, file));
+    });
+  } else if (fs.lstatSync(_path).isFile()) {
+    fileList.add(_path.replace(path.resolve(process.cwd(), "src"), ""));
+  }
+};
+
+const filter = (fileExtensionsReg: RegExp[]) => {
+  fileList.forEach((file) => {
+    for (const [index, reg] of fileExtensionsReg.entries()) {
+      if (reg.test(file)) break;
+      if (index === fileExtensionsReg.length - 1) fileList.delete(file);
+    }
+  });
+};
+
+export default (
+  paths: string[],
+  fileExtensionsReg: RegExp[],
+  timeout = 2000
+) => ({
+  name: "vite-plugin-preload",
+  transform(code: string, id: string) {
+    if (id.includes("src/index.ts")) {
+      paths.forEach((_path) => {
+        getSubDirectory(path.resolve(process.cwd(), _path));
+      });
+      filter(fileExtensionsReg);
+      console.log(
+        "%c [ fileList ]-5",
+        "font-size:13px; background:pink; color:#bf2c9f;",
+        fileList
+      );
+      return `
+          ${code};
+          window.onload = () => {
+              const lazyPages = ${JSON.stringify(Array.from(fileList))};
+              setTimeout(() => {
+                lazyPages.forEach(item => fetch(item));
+              }, ${timeout});
+          }
+      `;
+    }
+    return code;
+  },
+});
